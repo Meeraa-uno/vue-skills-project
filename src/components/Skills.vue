8570e5262e88b5fa57cf525f53863fdfc6ece006
Meeraa_uno <njayachandran@unomaha.edu>
2019-09-07 08:26:52 -0500

    chn

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,32 +1,75 @@
 <template>
-  <div class="hello">
-    sdfsfd
+  <div class="container">
+      <div class="holder">  
+        <form @submit.prevent="addSkill">
+        <input type="text" placeholder="Enter a skill you have.."  v-model="skill">
+        <!-- Add this line -->
+        <input type="checkbox" id="checkbox" v-model="checked">
+        <ul>
+          <li v-for="(data, index) in skills" :key='index'>{{data.skill}}</li>
+        </ul>
+        <p>These are the skills that you possess.</p>       
+    </form>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Skills',
-  props: {
-    
+  data() {
+    // Removed for brevity
+  },
+
+  // Add this section:
+  methods : {
+      addSkill(){
+          this.skills.push({skill: this.skill});
+          this.skill = '';
+      }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
+  .holder {
+    background: #fff;
+  }
+
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+  }
+  
+  ul li {
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #E0EDF4;
+    border-left: 5px solid #3EB3F6;
+    margin-bottom: 2px;
+    color: #3E5252;
+  }
+
+  p {
+    text-align:center;
+    padding: 30px 0;
+    color: gray;
+  }
+
+  .container {
+    box-shadow: 0px 0px 40px lightgray;
+  }
+
+  input {
+    width: calc(100% - 40px);
+    border: 0;
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #323333;
+    color: #687F7F;
+  }
+
 </style>
