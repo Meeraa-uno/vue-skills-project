9f9c10da08d08f3ae65cc1efd9e077d0bb68c99f
Meeraa_uno <njayachandran@unomaha.edu>
2019-09-13 18:33:37 -0500

    j

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -3,13 +3,25 @@
       <div class="holder">  
         
         <form @submit.prevent="addSkill">
-          <input type="text" placeholder="Enter a skill you have.." v-validate="'required'" v-model="skill" name="skill">
+          <input type="text" placeholder="Enter a skill you have.." v-model="skill" v-validate="'min:5'" name="skill">
           
+        
+          <transition name="alert-in" enter-active-class="animated flipInX" leave-active-class="animated flipOutX">
+            <p class="alert" v-if="errors.has('skill')">{{ errors.first('skill') }}</p>
+          </transition>
+        
         </form>
 
-        <ul>
+       <ul>
+        <transition-group name="list" enter-active-class="animated bounceInUp" leave-active-class="animated bounceOutDown">
           <li v-for="(data, index) in skills" :key='index'>{{data.skill}}</li>
-        </ul>
+          <li v-for="(data, index) in skills" :key='index'>{{data.skill}}
+          <i class="fa fa-minus-circle" v-on:click="remove(index)"></i>
+          </li>
+        </transition-group>
+      </ul>
+
+      
 
         <p>These are the skills that you possess.</p>       
     </div>
@@ -31,10 +43,15 @@ export default {
   // Add this section:
   methods : {
       addSkill() {
-       
+        this.$validator.validateAll().then((result) => {
+          if (result) {
             this.skills.push({skill: this.skill});
             this.skill = '';
-          
+          }
+        })
+      },
+      remove(id) {
+        this.skills.splice(id, 1);
       }
     }  
   } 
@@ -43,6 +60,8 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+@import "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"; 
+@import "https://cdn.jsdelivr.net/npm/animate.css@3.5.1";
 
   .holder {
     background: #fff;
@@ -87,6 +106,24 @@ export default {
     display: inline-block;
     padding: 5px;
     margin-top: -20px;
+  }
+
+  .alert-in-enter-active {
+  animation: bounce-in .5s;
+  }
+  .alert-in-leave-active {
+    animation: bounce-in .5s reverse;
+  }
+  @keyframes bounce-in {
+    0% {
+      transform: scale(0);
+    }
+    50% {
+      transform: scale(1.5);
+    }
+    100% {
+      transform: scale(1);
+    }
   }
 
 </style>
